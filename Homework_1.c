700908de3d635922229832545c8e3d54a4f24f89
Sudeep Kulkarni <suku8854@colorado.edu>
2017-02-04 04:32:26 -0700

    REAl

--- Homework_1.c
+++ Homework_1.c
@@ -2,34 +2,43 @@
 #include <stdint.h>
 
 
-signed char reverse(char * str, int length);
-uint8_t length(char*str);
+signed char reverse(signed char * str, uint8_t length);
+uint8_t leng(char*str);
 //int b=10;
-uint8_t Length =17;
+uint8_t Length=17;
+
+
 int main()
 {
     uint8_t len;
 	signed char A[]="This is a string";
+
 	len=leng(&A);
+
+
 	leng(&A);
-	//if (A[0] == '\0')
-        //printf("the first character is null hence m reversing after that\n");
+	if (A[0] == '\0')
+        printf("the first character is null hence m reversing after that\n");
 	printf("length %d\n", len);
     //if (b<len)
     //printf("The function will give erroneous results");
     if (len>0)
         printf("reversed\n");
 	reverse(&A,len);
+
+
 	return 0;
 }
 
 uint8_t leng(char*str)
 {
     uint8_t count=0;
-    while(*(str) != '\0')
+    while(*(str) !='\0')
         {
             count++;
             str++;
+        if (*(str) =='\0')
+            continue;
         }
      return count;
 }
@@ -37,47 +46,47 @@ uint8_t leng(char*str)
 signed char reverse(signed char * str, uint8_t length)
 {
     //if (length>0)
-    if (Length >length)
+    if (length<=Length)
     {
 
-	
+
 	for (int i=0; i<(length); i++)
 	{
-	    
+
 	    if (i<(length)/2)
-            printf("%c\n",*(str+(length-i)));
+            printf("%c",*(str+(length-i)));
         else if (i>(length)/2)
-            printf("%c\n",*(str+(length-i)));
+            printf("%c",*(str+(length-i)));
         else
-            printf("%c\n",*(str+i));
+            printf("%c",*(str+i));
 
 	}
     *(str+(length-1))=*str;
-    printf("%c\n",*(str+(length-1)));
+    printf("%c",*(str+(length-1)));
     }
     //else if (length <= 0)
         //printf("fail\n");
-    else 
+    //else
+	//
+    else
+    {
+
+	for (uint8_t i=0; i<(Length); i++)
 	{
-		
-	for (int i=0; i<(Length); i++)
-	{
-	    
+
 	    if (i<(Length)/2)
-            printf("%c\n",*(str+(Length-i)));
+            printf("%c",*(str+(Length-i)));
         else if (i>(length)/2)
-            printf("%c\n",*(str+(Length-i)));
+            printf("%c",*(str+(Length-i)));
         else
-            printf("%c\n",*(str+i));
+            printf("%c",*(str+i));
 
 	}
     *(str+(Length-1))=*str;
-    printf("%c\n",*(str+(Length-1)));
-    }
-	}
-	
-    //if (*(str+length)=='\0')
-        //printf("The function will reverse everything before the null character\n");
-    return 0;
-}
+    printf("%c",*(str+(Length-1)));
 
+	}
+
+	putchar ('\n');
+    return '0';
+}
